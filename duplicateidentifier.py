391cfcee03b8a598ad6df1c850822581b2598e5d
logzee <45110509+logzee@users.noreply.github.com>
2020-04-04 15:11:16 -0500

    Update duplicateidentifier.py

--- duplicateidentifier.py
+++ duplicateidentifier.py
@@ -1,17 +1,16 @@
 import nltk
 import pandas as pd
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
 from nltk import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import LancasterStemmer, WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
-from copy import deepcopy
-from fuzzywuzzy import fuzz
 
-root = '/Users/alinajam/Desktop/quora_duplicate_questions.tsv'
+root = '/Users/mcint/comp395/HW/DC4/quora_duplicate_questions.tsv'
 data = pd.read_csv(root, delimiter='\t',  engine='python')
 
 data = data.drop(['id', 'qid1', 'qid2'], axis=1)
-
 # length based features
 data['len_q1'] = data.question1.apply(lambda x: len(str(x)))
 data['len_q2'] = data.question2.apply(lambda x: len(str(x)))
@@ -37,10 +36,11 @@ len(set(str(x['question1'])
 .intersection(set(str(x['question2'])
 .lower().split()))), axis=1)
 
-fs_1 = ['len_q1', 'len_q2', 'diff_len', 'len_char_q1',
+fs_1 = data[['len_q1', 'len_q2', 'diff_len', 'len_char_q1',
         'len_char_q2', 'len_word_q1', 'len_word_q2',
-        'common_words']
+        'common_words']]
 
+print("Basic Features: ", fs_1)
 #fuzzyfeatures
 data['fuzz_qratio'] = data.apply(lambda x: fuzz.QRatio(
     str(x['question1']), str(x['question2'])), axis=1)
@@ -63,7 +63,8 @@ data['fuzz_token_set_ratio'] = data.apply(lambda x:
 fuzz.token_set_ratio(str(x['question1']),
 str(x['question2'])), axis=1)
 
-fs_2 = ['fuzz_qratio', 'fuzz_WRatio', 'fuzz_partial_ratio',
+fs_2 = data[['fuzz_qratio', 'fuzz_WRatio', 'fuzz_partial_ratio',
        'fuzz_partial_token_set_ratio', 'fuzz_partial_token_sort_ratio',
-       'fuzz_token_set_ratio', 'fuzz_token_sort_ratio']
+       'fuzz_token_set_ratio']]
 
+print("Fuzzy Features: ", fs_2)
